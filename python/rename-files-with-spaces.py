c3afe6647b1bb3f95adf9885fd0e848913d020f6
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2024-06-05 20:50:26 +0530

    rename files

--- python/rename-files-with-spaces.py
+++ python/rename-files-with-spaces.py
@@ -1,14 +1,38 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', required=True)
-args = parser.parse_args()
+def read_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', required=True)
 
-directory = args.directory.strip()
+    return parser.parse_args()
 
-for filename in os.listdir(directory):
-    new_filename = filename.replace(' ', '-').lower()
-    print(f'Renaming {filename} to {new_filename}')
-    if new_filename != filename:
-        os.rename(f'{directory}{filename}', f'{directory}{new_filename}')
+def rename_files_in_a_directory(directory):
+
+    for filename in os.listdir(directory):
+        new_filename = filename.replace(' ', '-').lower()
+
+        absolute_filename = os.path.join(directory, filename)
+        absolute_new_filename = os.path.join(directory, new_filename)
+        print(f'Renaming {absolute_filename} to {absolute_new_filename}')
+        if new_filename != filename:
+            os.rename(absolute_filename, absolute_new_filename)
+
+def rename_all_files(directory):
+    """
+        Rename all files in a directory and sub-directory and also rename the sub-directory
+    """
+
+    for path, folders, files in os.walk(directory):
+        for filename in files:
+            os.rename(os.path.join(path, filename), os.path.join(path, filename.replace(' ', '-').lower()))
+
+        for i in range(len(folders)):
+            new_folder_name = folders[i].replace(' ', '-').lower()
+            os.rename(os.path.join(path, folders[i]), os.path.join(path, new_folder_name))
+            folders[i] = new_folder_name
+
+if __name__ == '__main__':
+    args = read_args()
+    # rename_files_in_a_directory(args.directory)
+    rename_all_files(args.directory)
